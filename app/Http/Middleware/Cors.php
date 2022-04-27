0ba91f0d2c02f16aedd2a0c570a59701b144d519
Jesús Quishpe <jesusquishpe17@gmail.com>
2022-04-27 12:55:23 -0500

    Modificando cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,9 +17,9 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         $response = $next($request);
-        $response->headers->set('Access-Control-Allow-Origin', '*');
-        $response->headers->set('Access-Control-Allow-Methods', 'POST,GET,OPTIONS,PUT,DELETE');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type,Accept,Authorization,X-Requested-With,Application');
+        $response->header('Access-Control-Allow-Origin', '*');
+        $response->header('Access-Control-Allow-Methods', 'POST,GET,OPTIONS,PUT,DELETE');
+        $response->header('Access-Control-Allow-Headers', 'Content-Type,Accept,Authorization,X-Requested-With,Application');
         return $response;
     }
 }
