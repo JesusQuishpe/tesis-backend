9dedfb8cb5e163bb1406f685a2bc627d35c6d3b3
Jesús Quishpe <jesusquishpe17@gmail.com>
2022-04-27 09:17:24 -0500

    eliminando build carpeta

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,21 +2,22 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8" />
-  <link rel="icon" href="build/favicon.ico" />
-  <meta name="viewport" content="width=device-width,initial-scale=1" />
-  <meta name="theme-color" content="#000000" />
-  <meta name="description" content="Web site created using create-react-app" />
-  <link rel="apple-touch-icon" href="build/logo192.png" />
-  <link rel="manifest" href="build/manifest.json" />
-  <title>React App</title>
+    <meta charset="utf-8" />
+    <link rel="icon" href="build/favicon.ico" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <meta name="theme-color" content="#000000" />
+    <meta name="description" content="Web site created using create-react-app" />
+    <link rel="apple-touch-icon" href="build/logo192.png" />
+    <link rel="manifest" href="build/manifest.json" />
+    <title>React App</title>
 
-  <link href="build/static/css/main.b453595b.css" rel="stylesheet">
+    <link href="build/static/css/main.b453595b.css" rel="stylesheet">
 </head>
-  <div id="modal"></div>
-  <div id="toast"></div>
-  <div id="root"></div>
-  <script defer="defer" src="build/static/js/main.95094ebc.js"></script>
+<div id="modal"></div>
+<div id="toast"></div>
+<div id="root"></div>
+<script defer="defer" src="build/static/js/main.95094ebc.js"></script>
 
 </body>
+
 </html>
